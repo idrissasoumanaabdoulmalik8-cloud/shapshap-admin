8265e76a3b3b67b832f5f57d636909e2904cbb38
Abdoul Malik <idrissasoumanaabdoulmalik8@gmail.com>
2026-07-09 20:28:19 +0100

    Fix delete favorite @Transactional

--- src/main/java/shashap_backand/demo/repository/FavoriteRepository.java
+++ src/main/java/shashap_backand/demo/repository/FavoriteRepository.java
@@ -2,12 +2,16 @@ package shashap_backand.demo.repository;
 
 import shashap_backand.demo.entity.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByPhoneNumber(String phoneNumber);
     Optional<Favorite> findByPhoneNumberAndProductId(String phoneNumber, Long productId);
+
+    @Transactional
     void deleteByPhoneNumberAndProductId(String phoneNumber, Long productId);
+
     boolean existsByPhoneNumberAndProductId(String phoneNumber, Long productId);
 }
